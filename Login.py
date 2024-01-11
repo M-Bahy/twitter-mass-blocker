c448bfba3c5492e74ffa9afa24744aa269720f93
M-Bahy <Homelander.SolderBoy@gmail.com>
2024-01-11 18:25:34 +0200

    get author and some followers

--- Login.py
+++ Login.py
@@ -9,6 +9,9 @@ import time
 
 load_dotenv()
 
+tweet_author = ""
+targets = []
+
 
 def init_driver():
     chrome_path = os.getenv("chrome_path")
@@ -29,7 +32,7 @@ def delay(seconds):
 
 
 def login(driver):
-    driver.get("https://twitter.com/i/flow/login")
+    driver.get("https://x.com/i/flow/login")
     delay(2)
     username_field = driver.find_element(By.NAME, "text")
     username_field.click()
@@ -44,9 +47,46 @@ def login(driver):
     delay(2)
 
 
+def get_target(driver):
+    global tweet_author
+    driver.get(os.getenv("target_url"))
+    delay(5)
+    try:
+        target_username = driver.find_element(
+            By.XPATH,
+            "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[2]/div/div/article/div/div/div[2]/div[2]/div/div/div[1]/div/div/div[2]/div/div/a/div/span",
+        )
+    except Exception:
+        target_username = driver.find_element(
+            By.XPATH,
+            "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[1]/div/div/article/div/div/div[2]/div[2]/div/div/div[1]/div/div/div[2]/div/div/a/div/span",
+        )
+    tweet_author = target_username.text[1:]
+    targets.append(tweet_author)
+
+
+def get_followers(driver):
+    driver.get(f"https://x.com/{tweet_author}/followers")
+    delay(5)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    delay(2)
+    driver.execute_script("window.scrollTo(0, 0);")
+    delay(2)
+    followers = driver.find_elements(
+        By.XPATH,
+        "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[*]/div/div/div/div/div[2]/div[1]/div[1]/div/div[2]/div/a/div/div/span",
+    )
+    print(len(followers))
+    for follower in followers:
+        print(follower.text)
+        targets.append(follower.text[1])
+
+
 def main():
     driver = init_driver()
     login(driver)
+    get_target(driver)
+    get_followers(driver)
     keep_open(driver)
 
 
